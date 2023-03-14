a67c4f1147813e2051cd0ec24e392c6541679af5
YO4 <ysno@ac.auone-net.jp>
2023-03-14 19:54:18 +0900

    save tracing logs as artifacts

--- test_rm_and_mv.cmd
+++ test_rm_and_mv.cmd
@@ -17,9 +17,9 @@ sh -x -c "find /c/normal_dir -type f"
 echo;
 echo === normal directory : test
 
-sh -x -c "rm /c/normal_dir/file_to_unlink"
+sh -x -c "strace -o /c/opt/rm_on_normal_dir.trace rm /c/normal_dir/file_to_unlink"
 call :a
-sh -x -c "mv /c/normal_dir/file_to_rename /c/normal_dir/rename_complete"
+sh -x -c "strace -o /c/opt/mv_on_normal_dir.trace mv /c/normal_dir/file_to_rename /c/normal_dir/rename_complete"
 call :a
 sh -x -c "find /c/normal_dir -type f"
 
@@ -36,9 +36,9 @@ sh -x -c "find /c/binded_dir -type f"
 echo;
 echo === bind mounted directory : test
 
-sh -x -c "rm /c/binded_dir/file_to_unlink"
+sh -x -c "strace -o /c/opt/rm_on_mounted_dir.trace rm /c/binded_dir/file_to_unlink"
 call :a
-sh -x -c "mv /c/binded_dir/file_to_rename /c/binded_dir/rename_complete"
+sh -x -c "strace -o /c/opt/mv_on_mounted_dir.trace mv /c/binded_dir/file_to_rename /c/binded_dir/rename_complete"
 call :a
 sh -x -c "find /c/binded_dir -type f"
 
@@ -56,9 +56,9 @@ sh -x -c "(find /c/normal_dir /c/binded_dir -type f)"
 echo;
 echo === crossing directory mv : test
 
-sh -x -c "mv /c/normal_dir/normal_to_bind /c/binded_dir/to_binded_complete"
+sh -x -c "strace -o /c/opt/mv_normal_dir_to_mounted_dir.trace mv /c/normal_dir/normal_to_bind /c/binded_dir/to_binded_complete"
 call :a
-sh -x -c "mv /c/binded_dir/binded_to_normal /c/normal_dir/to_normal_complete"
+sh -x -c "strace -o /c/opt/mv_mounted_dir_to_normal_dir.trace mv /c/binded_dir/binded_to_normal /c/normal_dir/to_normal_complete"
 call :a
 sh -x -c "(find /c/normal_dir /c/binded_dir -type f)"
 
@@ -72,6 +72,7 @@ exit /b %result%
 
 rem accumulate errorlevel
 :a
+if not errorlevel 1 exit /b
 echo [41mfailed.[m
 set /a result=%result%+%errorlevel%
 exit /b
